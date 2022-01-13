6765aa2fc7ac637a0a5c16b1d390ddf17a06b0cd
augustinewafula <augustinetreezy@outlook.com>
2022-01-13 13:07:55 +0300

    create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'George Kimani';
+        $user->email = 'george@progressive.co.ke';
+        $user->password = bcrypt('qwertyuiop');
+        $role = Role::create(['name' => 'admin']);
+        $user->assignRole($role);
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Augustine Wafula';
+        $user->email = 'augustinetreezy@gmail.com';
+        $user->password = bcrypt('password');
+        $role = Role::create(['name' => 'user']);
+        $user->assignRole($role);
+        $user->save();
+    }
+}
