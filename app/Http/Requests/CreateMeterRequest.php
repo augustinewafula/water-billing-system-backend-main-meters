c054e531fc6f8d8c39246de8a7e12a158d03067d
augustinewafula <augustinetreezy@outlook.com>
2022-01-21 13:29:53 +0300

    create meter request. add validation rules

---
+++ app/Http/Requests/CreateMeterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\ValveStatus;
+use BenSampo\Enum\Rules\EnumValue;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateMeterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'number' => ['required', 'numeric', 'max:255'],
+            'valve_status' => ['required', new EnumValue(ValveStatus::class)],
+            'station_id' => ['required', 'string', 'exists:stations,id'],
+            'type' => ['sometimes', 'required', 'exists:meter_types']
+        ];
+    }
+}
