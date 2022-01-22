2d1e1df64d26190140d44e906b706b61ee8b4a06
augustinewafula <augustinetreezy@outlook.com>
2022-01-22 20:02:51 +0300

    create update meter request

---
+++ app/Http/Requests/UpdateMeterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\ValveStatus;
+use BenSampo\Enum\Rules\EnumValue;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateMeterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'number' => ['required', 'numeric'],
+            'valve_status' => ['required', new EnumValue(ValveStatus::class, false)],
+            'station_id' => ['required', 'string', 'exists:meter_stations,id'],
+            'type' => ['sometimes', 'required', 'exists:meter_types,id']
+        ];
+    }
+}
