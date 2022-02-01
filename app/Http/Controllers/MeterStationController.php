909b98ee449a11259d4ba6095f044e1f334504d4
augustinewafula <augustinetreezy@outlook.com>
2022-02-01 14:32:53 +0300

    get all meter stations

--- app/Http/Controllers/MeterStationController.php
+++ app/Http/Controllers/MeterStationController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MeterStation;
+
 class MeterStationController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(MeterStation::all());
+    }
 }
