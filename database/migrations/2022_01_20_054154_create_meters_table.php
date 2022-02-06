205cf27034043261b8733beee03babbc47557003
augustinewafula <augustinetreezy@outlook.com>
2022-02-06 11:29:10 +0300

    remove valve_status default value and set to nullable

--- database/migrations/2022_01_20_054154_create_meters_table.php
+++ database/migrations/2022_01_20_054154_create_meters_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Enums\MeterMode;
-use App\Enums\ValveStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,7 +17,7 @@ class CreateMetersTable extends Migration
         Schema::create('meters', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('number')->unique();
-            $table->tinyInteger('valve_status')->unsigned()->default(ValveStatus::Open);
+            $table->tinyInteger('valve_status')->unsigned()->nullable();
             $table->uuid('station_id');
             $table->foreign('station_id')->references('id')->on('meter_stations');
             $table->tinyInteger('mode')->unsigned()->default(MeterMode::Manual);
