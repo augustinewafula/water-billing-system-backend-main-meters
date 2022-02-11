4384ed9c0474ae02460dbfda2366e8ab96ea6741
augustinewafula <augustinetreezy@outlook.com>
2022-02-11 11:40:00 +0300

    create meter billing migration

---
+++ database/migrations/2022_02_11_083231_create_meter_billings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeterBillingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meter_billings', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('meter_reading_id');
+            $table->foreign('meter_reading_id')->references('id')->on('meter_readings');
+            $table->decimal('amount_paid', 15);
+            $table->decimal('balance', 15)->default(0);
+            $table->string('date_paid')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meter_billings');
+    }
+}
