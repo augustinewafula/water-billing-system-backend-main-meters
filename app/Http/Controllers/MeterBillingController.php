803f23fd0f3dfe7a5aed3c2ca57032fffdf5185e
augustinewafula <augustinetreezy@outlook.com>
2022-02-11 11:39:43 +0300

    create meter billing controller

---
+++ app/Http/Controllers/MeterBillingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MeterBilling;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class MeterBillingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param MeterBilling $meterBilling
+     * @return Response
+     */
+    public function show(MeterBilling $meterBilling)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param MeterBilling $meterBilling
+     * @return Response
+     */
+    public function update(Request $request, MeterBilling $meterBilling)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param MeterBilling $meterBilling
+     * @return Response
+     */
+    public function destroy(MeterBilling $meterBilling)
+    {
+        //
+    }
+}
