718c0d3895a93f4e9c78d270bc1cfa13b5e078c7
augustinewafula <augustinetreezy@outlook.com>
2022-02-11 21:45:38 +0300

    create meter billing reports controller

---
+++ app/Http/Controllers/MeterBillingReportController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MeterBillingReport;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class MeterBillingReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param MeterBillingReport $meterBillingReport
+     * @return Response
+     */
+    public function show(MeterBillingReport $meterBillingReport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param MeterBillingReport $meterBillingReport
+     * @return Response
+     */
+    public function update(Request $request, MeterBillingReport $meterBillingReport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param MeterBillingReport $meterBillingReport
+     * @return Response
+     */
+    public function destroy(MeterBillingReport $meterBillingReport)
+    {
+        //
+    }
+}
