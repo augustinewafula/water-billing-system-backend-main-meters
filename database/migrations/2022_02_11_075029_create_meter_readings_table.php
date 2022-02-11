b07b7b657acea6de815984b9c540a94473ad23e0
augustinewafula <augustinetreezy@outlook.com>
2022-02-11 11:29:15 +0300

    create migration for meter readings

---
+++ database/migrations/2022_02_11_075029_create_meter_readings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\MeterReadingStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeterReadingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meter_readings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('previous_reading');
+            $table->integer('current_reading');
+            $table->string('month');
+            $table->decimal('bill', 15);
+            $table->tinyInteger('status')->unsigned()->default(MeterReadingStatus::NotPaid);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meter_readings');
+    }
+}
