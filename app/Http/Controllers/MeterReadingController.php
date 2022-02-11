7b4611d0bfeb6d57d88bf78ebb2a8ee7efc996c4
augustinewafula <augustinetreezy@outlook.com>
2022-02-11 11:30:59 +0300

    create meter reading controller

---
+++ app/Http/Controllers/MeterReadingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MeterReading;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class MeterReadingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param MeterReading $meterReading
+     * @return Response
+     */
+    public function show(MeterReading $meterReading)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param MeterReading $meterReading
+     * @return Response
+     */
+    public function update(Request $request, MeterReading $meterReading)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param MeterReading $meterReading
+     * @return Response
+     */
+    public function destroy(MeterReading $meterReading)
+    {
+        //
+    }
+}
