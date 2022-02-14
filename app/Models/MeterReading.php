f9b1b54d9a53ba13b27256c61ac257cc57646c7e
augustinewafula <augustinetreezy@outlook.com>
2022-02-14 14:28:46 +0300

    add belongs to meter relationship on meter reading model

--- app/Models/MeterReading.php
+++ app/Models/MeterReading.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MeterReading extends Model
 {
@@ -15,4 +16,13 @@ class MeterReading extends Model
     protected $keyType = 'uuid';
 
     protected $fillable = ['meter_id', 'previous_reading', 'current_reading', 'month', 'bill', 'status'];
+
+    /**
+     * Get meter that owns the meter reading
+     * @return BelongsTo
+     */
+    public function meter(): BelongsTo
+    {
+        return $this->belongsTo(Meter::class);
+    }
 }
