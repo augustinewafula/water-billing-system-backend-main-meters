28bb4008429720a993744b4c2071991ab5c4b2a7
augustinewafula <augustinetreezy@outlook.com>
2022-02-17 21:55:50 +0300

    extract store meter reading method to a trait

--- app/Http/Controllers/MeterReadingController.php
+++ app/Http/Controllers/MeterReadingController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateMeterReadingRequest;
 use App\Http\Requests\UpdateMeterReadingRequest;
 use App\Models\Meter;
-use App\Models\MeterCharge;
 use App\Models\MeterReading;
-use Carbon\Carbon;
+use App\Traits\StoreMeterReading;
 use DB;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
@@ -19,6 +17,7 @@ use Throwable;
 
 class MeterReadingController extends Controller
 {
+    use StoreMeterReading;
     /**
      * Display a listing of the resource.
      *
@@ -39,49 +38,7 @@ class MeterReadingController extends Controller
         return response()->json($meter_readings);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param CreateMeterReadingRequest $request
-     * @return Application|ResponseFactory|JsonResponse|Response
-     * @throws Throwable
-     */
-    public function store(CreateMeterReadingRequest $request)
-    {
-        $meter = Meter::find($request->meter_id);
-        $bill = $this->calculateBill($meter->last_reading, $request->current_reading);
 
-        try {
-            DB::transaction(static function () use ($request, $bill, $meter) {
-                MeterReading::create([
-                    'meter_id' => $request->meter_id,
-                    'previous_reading' => $meter->last_reading,
-                    'current_reading' => $request->current_reading,
-                    'month' => $request->month,
-                    'bill' => $bill
-                ]);
-                $meter->update([
-                    'last_reading' => $request->current_reading,
-                    'last_reading_date' => Carbon::now()->toDateTimeString(),
-                ]);
-            });
-        } catch (Throwable $th) {
-            Log::error($th);
-            $response = ['message' => 'Something went wrong, please contact website admin for help'];
-            return response($response, 422);
-        }
-        return response()->json($bill, 201);
-
-    }
-
-    public function calculateBill($previous_reading, $current_reading)
-    {
-        $meter_charges = MeterCharge::take(1)
-            ->first();
-        return (
-                ($current_reading - $previous_reading) * $meter_charges->cost_per_unit)
-            + $meter_charges->service_charge;
-    }
 
     public function calculateBillUpdate($previous_reading, $current_reading, $previous_bill)
     {
---
+++ app/Traits/StoreMeterReading.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Traits;
+
+use App\Http\Requests\CreateMeterReadingRequest;
+use App\Models\Meter;
+use App\Models\MeterCharge;
+use App\Models\MeterReading;
+use Carbon\Carbon;
+use DB;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Log;
+use Throwable;
+
+trait StoreMeterReading
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param CreateMeterReadingRequest $request
+     * @return Application|ResponseFactory|JsonResponse|Response
+     * @throws Throwable
+     */
+    public function store(CreateMeterReadingRequest $request)
+    {
+        $meter = Meter::find($request->meter_id);
+        $bill = $this->calculateBill($meter->last_reading, $request->current_reading);
+
+        try {
+            DB::transaction(static function () use ($request, $bill, $meter) {
+                MeterReading::create([
+                    'meter_id' => $request->meter_id,
+                    'previous_reading' => $meter->last_reading,
+                    'current_reading' => $request->current_reading,
+                    'month' => $request->month,
+                    'bill' => $bill
+                ]);
+                $meter->update([
+                    'last_reading' => $request->current_reading,
+                    'last_reading_date' => Carbon::now()->toDateTimeString(),
+                ]);
+            });
+        } catch (Throwable $th) {
+            Log::error($th);
+            $response = ['message' => 'Something went wrong, please contact website admin for help'];
+            return response($response, 422);
+        }
+        return response()->json($bill, 201);
+
+    }
+
+    public function calculateBill($previous_reading, $current_reading)
+    {
+        $meter_charges = MeterCharge::take(1)
+            ->first();
+        return (
+                ($current_reading - $previous_reading) * $meter_charges->cost_per_unit)
+            + $meter_charges->service_charge;
+    }
+}
