8edd1fda7927f84e8def89643bc0731b771c38de
augustinewafula <augustinetreezy@outlook.com>
2022-02-21 09:07:04 +0300

    create meter token model and migration

---
+++ database/migrations/2022_02_21_043109_create_meter_tokens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeterTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meter_tokens', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('mpesa_transaction_id')->nullable()->constrained();
+            $table->string('token');
+            $table->string('units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meter_tokens');
+    }
+}
