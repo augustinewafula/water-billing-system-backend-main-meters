9296cbe262e3285bbd257ef7ea7c7d8b9eea76bb
augustinewafula <augustinetreezy@outlook.com>
2022-02-24 06:53:27 +0300

    get transactions from both billings and tokens

--- app/Http/Controllers/MeterBillingController.php
+++ app/Http/Controllers/MeterBillingController.php
@@ -18,6 +18,8 @@ use App\Models\User;
 use App\Traits\ProcessPrepaidMeterTransaction;
 use Carbon\Carbon;
 use DB;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -33,21 +35,20 @@ class MeterBillingController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param Request $request
      * @return JsonResponse
      */
     public function index(Request $request): JsonResponse
     {
-        $meter_billings = MeterBilling::query();
-        $meter_billings = $meter_billings->select('mpesa_transactions.id', 'meter_billings.mpesa_transaction_id', 'mpesa_transactions.TransID as transaction_reference', 'mpesa_transactions.TransAmount as amount', 'mpesa_transactions.MSISDN as phone_number', 'mpesa_transactions.created_at as transaction_time')
-            ->join('mpesa_transactions', 'mpesa_transactions.id', 'meter_billings.mpesa_transaction_id')
-            ->join('meter_readings', 'meter_readings.id', 'meter_billings.meter_reading_id')
-            ->join('meters', 'meters.id', 'meter_readings.meter_id')
-            ->join('meter_stations', 'meter_stations.id', 'meters.station_id');
-        if ($request->has('station_id')) {
-            $meter_billings = $meter_billings->where('meter_stations.id', $request->query('station_id'));
-        }
-        $meter_billings = $meter_billings->get();
-        return response()->json($meter_billings);
+        $meter_billings_transactions = MeterBilling::query();
+        $meter_billings_transactions = $this->getTransactions($meter_billings_transactions, $request, 'billing');
+
+        $meter_tokens_transactions = MeterToken::query();
+        $meter_tokens_transactions = $this->getTransactions($meter_tokens_transactions, $request, 'tokens');
+
+        $all_transactions = $meter_billings_transactions->merge($meter_tokens_transactions);
+
+        return response()->json($all_transactions);
     }
 
     /**
@@ -69,6 +70,7 @@ class MeterBillingController extends Controller
      * Store a newly created resource in storage.
      *
      * @param CreateMeterBillingRequest $request
+     * @param $mpesa_transaction_id
      * @return JsonResponse
      */
     public function store(CreateMeterBillingRequest $request, $mpesa_transaction_id): JsonResponse
@@ -302,5 +304,31 @@ class MeterBillingController extends Controller
             'amount_paid' => $content->TransAmount
         ]);
         $this->store($request, $mpesa_transaction_id);
+    }
+
+    /**
+     * @param Builder $transactions
+     * @param Request $request
+     * @param $type
+     * @return void
+     */
+    public function getTransactions(Builder $transactions, Request $request, $type): Collection
+    {
+        if ($type === 'billing') {
+            $transactions = $transactions->select('mpesa_transactions.id', 'meter_billings.mpesa_transaction_id', 'mpesa_transactions.TransID as transaction_reference', 'mpesa_transactions.TransAmount as amount', 'mpesa_transactions.MSISDN as phone_number', 'mpesa_transactions.created_at as transaction_time')
+                ->join('mpesa_transactions', 'mpesa_transactions.id', 'meter_billings.mpesa_transaction_id')
+                ->join('meter_readings', 'meter_readings.id', 'meter_billings.meter_reading_id')
+                ->join('meters', 'meters.id', 'meter_readings.meter_id');
+        } else {
+            $transactions = $transactions->select('mpesa_transactions.id', 'meter_tokens.mpesa_transaction_id', 'mpesa_transactions.TransID as transaction_reference', 'mpesa_transactions.TransAmount as amount', 'mpesa_transactions.MSISDN as phone_number', 'mpesa_transactions.created_at as transaction_time')
+                ->join('mpesa_transactions', 'mpesa_transactions.id', 'meter_tokens.mpesa_transaction_id')
+                ->join('meters', 'meters.id', 'meter_tokens.meter_id');
+        }
+
+        $transactions = $transactions->join('meter_stations', 'meter_stations.id', 'meters.station_id');
+        if ($request->has('station_id')) {
+            $transactions = $transactions->where('meter_stations.id', $request->query('station_id'));
+        }
+        return $transactions->get();
     }
 }
