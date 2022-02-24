e480cebb3b31d2ec060d43c8d63209f9d855ac76
augustinewafula <augustinetreezy@outlook.com>
2022-02-24 10:31:02 +0300

    remove transaction index method from billing controller

--- app/Http/Controllers/MeterBillingController.php
+++ app/Http/Controllers/MeterBillingController.php
@@ -18,8 +18,6 @@ use App\Models\User;
 use App\Traits\ProcessPrepaidMeterTransaction;
 use Carbon\Carbon;
 use DB;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -31,25 +29,6 @@ use Throwable;
 class MeterBillingController extends Controller
 {
     use ProcessPrepaidMeterTransaction;
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function index(Request $request): JsonResponse
-    {
-        $meter_billings_transactions = MeterBilling::query();
-        $meter_billings_transactions = $this->getTransactions($meter_billings_transactions, $request, 'billing');
-
-        $meter_tokens_transactions = MeterToken::query();
-        $meter_tokens_transactions = $this->getTransactions($meter_tokens_transactions, $request, 'tokens');
-
-        $all_transactions = $meter_billings_transactions->merge($meter_tokens_transactions);
-
-        return response()->json($all_transactions);
-    }
 
     /**
      * @throws JsonException
@@ -141,44 +120,11 @@ class MeterBillingController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param MeterBilling $meterBilling
-     * @return Response
-     */
-    public function show(MeterBilling $meterBilling)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param MeterBilling $meterBilling
-     * @return Response
-     */
-    public function update(Request $request, MeterBilling $meterBilling)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param MeterBilling $meterBilling
-     * @return Response
-     */
-    public function destroy(MeterBilling $meterBilling)
-    {
-        //
-    }
-
-    /**
      * @param $amount_paid
      * @param $balance
      * @param $user
      * @param $meter_reading
+     * @param $mpesa_transaction_id
      * @return bool
      */
     public function saveBillingInfo(
@@ -305,31 +251,5 @@ class MeterBillingController extends Controller
             'amount_paid' => $content->TransAmount
         ]);
         $this->store($request, $mpesa_transaction_id);
-    }
-
-    /**
-     * @param Builder $transactions
-     * @param Request $request
-     * @param $type
-     * @return void
-     */
-    public function getTransactions(Builder $transactions, Request $request, $type): Collection
-    {
-        if ($type === 'billing') {
-            $transactions = $transactions->select('mpesa_transactions.id', 'meter_billings.mpesa_transaction_id', 'mpesa_transactions.TransID as transaction_reference', 'mpesa_transactions.TransAmount as amount', 'mpesa_transactions.MSISDN as phone_number', 'mpesa_transactions.created_at as transaction_time')
-                ->join('mpesa_transactions', 'mpesa_transactions.id', 'meter_billings.mpesa_transaction_id')
-                ->join('meter_readings', 'meter_readings.id', 'meter_billings.meter_reading_id')
-                ->join('meters', 'meters.id', 'meter_readings.meter_id');
-        } else {
-            $transactions = $transactions->select('mpesa_transactions.id', 'meter_tokens.mpesa_transaction_id', 'mpesa_transactions.TransID as transaction_reference', 'mpesa_transactions.TransAmount as amount', 'mpesa_transactions.MSISDN as phone_number', 'mpesa_transactions.created_at as transaction_time')
-                ->join('mpesa_transactions', 'mpesa_transactions.id', 'meter_tokens.mpesa_transaction_id')
-                ->join('meters', 'meters.id', 'meter_tokens.meter_id');
-        }
-
-        $transactions = $transactions->join('meter_stations', 'meter_stations.id', 'meters.station_id');
-        if ($request->has('station_id')) {
-            $transactions = $transactions->where('meter_stations.id', $request->query('station_id'));
-        }
-        return $transactions->get();
     }
 }
