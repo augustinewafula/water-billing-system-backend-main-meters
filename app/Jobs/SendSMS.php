5641efeb73153c22652d526d8ae63d80454bd582
augustinewafula <augustinetreezy@outlook.com>
2022-02-24 06:02:13 +0300

    update SendSms job to call SendSms trait

--- app/Jobs/SendSMS.php
+++ app/Jobs/SendSMS.php
@@ -2,7 +2,7 @@
 
 namespace App\Jobs;
 
-use Http;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -11,7 +11,7 @@ use Illuminate\Queue\SerializesModels;
 
 class SendSMS implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, \App\Traits\SendSms;
 
     protected $to, $message;
 
@@ -30,27 +30,11 @@ class SendSMS implements ShouldQueue
      * Execute the job.
      *
      * @return void
+     * @throws Exception
      */
     public function handle(): void
     {
-        env('AFRICASTKNG_USERNAME') === 'sandbox' ?
-            $url = 'https://api.sandbox.africastalking.com/version1/messaging' :
-            $url = 'https://api.africastalking.com/version1/messaging';
-        $sms_details = [
-            'username' => env('AFRICASTKNG_USERNAME'),
-            'to' => $this->to,
-            'message' => $this->message,
-        ];
-        if (!empty(env('AFRICASTKNG_SENDER_ID'))) {
-            $sms_details = array_merge($sms_details, ['from' => env('AFRICASTKNG_SENDER_ID')]);
-        }
-        Http::withHeaders([
-            'apiKey' => env('AFRICASTKNG_APIKEY'),
-            'Accept' => 'application/json'
-        ])
-            ->asForm()
-            ->retry(3, 100)
-            ->post($url,);
+        $this->initiateSendSms($this->to, $this->message);
     }
 
 }
