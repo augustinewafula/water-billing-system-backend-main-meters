f71a7038cac054392d227a539873edc647d0634f
augustinewafula <augustinetreezy@outlook.com>
2022-02-26 14:45:41 +0300

    send sms to user after saving meter reading

--- app/Traits/StoreMeterReading.php
+++ app/Traits/StoreMeterReading.php
@@ -3,9 +3,10 @@
 namespace App\Traits;
 
 use App\Http\Requests\CreateMeterReadingRequest;
+use App\Jobs\SendSMS;
 use App\Models\Meter;
-use App\Models\MeterCharge;
 use App\Models\MeterReading;
+use App\Models\User;
 use Carbon\Carbon;
 use DB;
 use Illuminate\Contracts\Foundation\Application;
@@ -17,6 +18,7 @@ use Throwable;
 
 trait StoreMeterReading
 {
+    use calculateBill;
     /**
      * Store a newly created resource in storage.
      *
@@ -30,38 +32,46 @@ trait StoreMeterReading
         $bill = $this->calculateBill($meter->last_reading, $request->current_reading);
 
         try {
-            DB::transaction(static function () use ($request, $bill, $meter) {
-                MeterReading::create([
-                    'meter_id' => $request->meter_id,
-                    'previous_reading' => $meter->last_reading,
-                    'current_reading' => $request->current_reading,
-                    'month' => $request->month,
-                    'bill' => $bill,
-                    'service_fee' => MeterCharge::take(1)->first()->service_charge
-                ]);
-                $meter->update([
-                    'last_reading' => $request->current_reading,
-                    'last_reading_date' => Carbon::now()->toDateTimeString(),
-                ]);
-            });
+            DB::beginTransaction();
+            $previous_reading = $meter->last_reading;
+            MeterReading::create([
+                'meter_id' => $request->meter_id,
+                'previous_reading' => $meter->last_reading,
+                'current_reading' => $request->current_reading,
+                'month' => $request->month,
+                'bill' => $bill,
+                'service_fee' => $this->calculateServiceFee($bill, 'post-pay')
+            ]);
+            $meter->update([
+                'last_reading' => $request->current_reading,
+                'last_reading_date' => Carbon::now()->toDateTimeString(),
+            ]);
+            DB::commit();
         } catch (Throwable $th) {
+            DB::rollBack();
             Log::error($th);
             $response = ['message' => 'Something went wrong, please contact website admin for help'];
             return response($response, 422);
         }
+        $this->sendMeterReadingsToUser($meter, $request, $bill, $previous_reading);
         return response()->json($bill, 201);
 
     }
 
-    public function calculateBill($previous_reading, $current_reading): float
+    public function sendMeterReadingsToUser($meter, $request, $bill, $previous_reading): bool
     {
-        $meter_charges = MeterCharge::where('for', 'prepay')
+        $user = User::where('meter_id', $meter->id)
             ->first();
-        $bill = ($current_reading - $previous_reading) * $meter_charges->cost_per_unit;
-        $service_charge = $meter_charges->service_charge;
-        if ($meter_charges->service_charge_in_percentage) {
-            $service_charge = ($service_charge * $bill) / 100;
+        if (!$user) {
+            return false;
         }
-        return round($bill + $service_charge);
+        $user_name = ucwords($user->name);
+        $next_month = Carbon::now()->add(1, 'month')->format('M');
+        $due_date = Carbon::parse('4th ' . $next_month)->format('d/m/Y');
+        $current_month = Carbon::now()->isoFormat('MMMM YYYY');
+        $units_consumed = $request->current_reading - $previous_reading;
+        $message = "Hello $user_name, your water billing for $current_month is as follows:\nReading: $request->current_reading\nPrevious reading: $previous_reading\nUnits consumed: $units_consumed\nBill: Ksh $bill\nBalance brought forward: Ksh $user->account_balance\nDue date: $due_date\nPay via paybill number 994470, account number $meter->number";
+        SendSMS::dispatch($user->phone, $message);
+        return true;
     }
 }
