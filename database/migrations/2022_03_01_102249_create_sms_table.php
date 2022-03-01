0b02fca266a90c9a5749b8feb91004c0fef5ba13
augustinewafula <augustinetreezy@outlook.com>
2022-03-01 13:43:12 +0300

    create sms table

---
+++ database/migrations/2022_03_01_102249_create_sms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('phone');
+            $table->string('message');
+            $table->string('status');
+            $table->decimal('cost', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sms');
+    }
+}
