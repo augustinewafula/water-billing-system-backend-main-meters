5637624795e630f193e628e662619222ef3b5bc6
augustinewafula <augustinetreezy@outlook.com>
2022-03-02 14:20:32 +0300

    create daily meter readings

---
+++ database/migrations/2022_03_02_070215_create_daily_meter_readings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDailyMeterReadingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daily_meter_readings', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->integer('reading');
+            $table->foreignUuid('meter_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daily_meter_readings');
+    }
+}
