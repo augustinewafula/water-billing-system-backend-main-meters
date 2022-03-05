01a159f784c6fd75d38d9ee0d8c2070cd805a7c3
augustinewafula <augustinetreezy@outlook.com>
2022-03-05 14:56:05 +0300

    create setting model and migration

---
+++ app/Models/Setting.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['bill_due_days', 'meter_reading_sms_delay_days'];
+}
