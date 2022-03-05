967341519863a7025c272c6e019b0efc2ff0cfc8
augustinewafula <augustinetreezy@outlook.com>
2022-03-05 14:58:12 +0300

    add bill_due_at to fillables

--- app/Models/MeterReading.php
+++ app/Models/MeterReading.php
@@ -16,7 +16,7 @@ class MeterReading extends Model
 
     protected $keyType = 'uuid';
 
-    protected $fillable = ['meter_id', 'previous_reading', 'current_reading', 'month', 'bill', 'service_fee', 'status', 'sms_sent', 'send_sms_at'];
+    protected $fillable = ['meter_id', 'previous_reading', 'current_reading', 'month', 'bill', 'service_fee', 'status', 'sms_sent', 'send_sms_at', 'bill_due_at'];
 
     /**
      * The attributes that should be cast.
