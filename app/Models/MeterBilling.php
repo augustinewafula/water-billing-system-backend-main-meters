da08a783d61ab80bdcb783e4a22057d864b10ea9
augustinewafula <augustinetreezy@outlook.com>
2022-03-16 16:43:43 +0300

    set belongs to meter readings

--- app/Models/MeterBilling.php
+++ app/Models/MeterBilling.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MeterBilling extends Model
 {
@@ -15,4 +16,13 @@ class MeterBilling extends Model
     protected $keyType = 'uuid';
 
     protected $fillable = ['meter_reading_id', 'amount_paid', 'balance', 'mpesa_transaction_id', 'date_paid'];
+
+    /**
+     * Get meter reading that owns the meter billing
+     * @return BelongsTo
+     */
+    public function meter_reading(): BelongsTo
+    {
+        return $this->belongsTo(MeterBilling::class);
+    }
 }
