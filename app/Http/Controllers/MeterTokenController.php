c9dca4a603fad470bfa88232b6fe2ef607a19a2a
augustinewafula <augustinetreezy@outlook.com>
2022-03-17 09:35:48 +0300

    remove unused methods

--- app/Http/Controllers/MeterTokenController.php
+++ app/Http/Controllers/MeterTokenController.php
@@ -64,17 +64,6 @@ class MeterTokenController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param int $id
@@ -85,26 +74,4 @@ class MeterTokenController extends Controller
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
