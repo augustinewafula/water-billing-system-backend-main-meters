d3b7ff9825cd0d44d49d91bde0330749d5a86925
augustinewafula <augustinetreezy@outlook.com>
2022-03-17 23:50:32 +0300

    filter index queries

--- app/Http/Controllers/MeterStationController.php
+++ app/Http/Controllers/MeterStationController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\MeterStation;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Log;
+use Str;
+use Throwable;
 
 class MeterStationController extends Controller
 {
-    public function index()
+    public function index(Request $request): JsonResponse
     {
-        return response()->json(MeterStation::latest()->get());
+        $meter_stations = MeterStation::query();
+        try {
+            $meter_stations = $this->filterQuery($request, $meter_stations);
+        } catch (Throwable $throw) {
+            Log::error($throw);
+        }
+        return response()->json($meter_stations);
+    }
+
+    private function filterQuery(Request $request, Builder $meter_stations)
+    {
+        $search = $request->query('search');
+        $sortBy = $request->query('sortBy');
+        $sortOrder = $request->query('sortOrder');
+
+        if ($request->has('search') && Str::length($search) > 0) {
+            $meter_stations->where('name', 'like', '%' . $search . '%');
+        }
+        if ($request->has('sortBy')) {
+            $meter_stations = $meter_stations->orderBy($sortBy, $sortOrder);
+        }
+        if ($request->has('paginate')) {
+            return $meter_stations->withCount('meters')
+                ->paginate(10);
+        }
+        return $meter_stations->get();
     }
 }
