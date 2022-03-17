415adf63f30d31621d845ff6d47dc1f3de700ba0
augustinewafula <augustinetreezy@outlook.com>
2022-03-17 09:34:37 +0300

    define model relationship

--- app/Models/MeterToken.php
+++ app/Models/MeterToken.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MeterToken extends Model
 {
@@ -15,4 +16,22 @@ class MeterToken extends Model
     protected $keyType = 'uuid';
 
     protected $fillable = ['mpesa_transaction_id', 'token', 'service_fee', 'meter_id', 'units'];
+
+    /**
+     * Get meter that owns the meter reading
+     * @return BelongsTo
+     */
+    public function meter(): BelongsTo
+    {
+        return $this->belongsTo(Meter::class);
+    }
+
+    /**
+     * Get mpesa transaction that owns the meter token
+     * @return belongsTo
+     */
+    public function mpesa_transaction(): belongsTo
+    {
+        return $this->belongsTo(MpesaTransaction::class);
+    }
 }
