080dce845cec21a7f741f2d9d61800754c3c5bba
augustinewafula <augustinetreezy@outlook.com>
2022-03-19 17:27:55 +0300

    define sms model relationship

--- app/Models/Sms.php
+++ app/Models/Sms.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Scout\Searchable;
 
 class Sms extends Model
@@ -16,4 +17,13 @@ class Sms extends Model
     protected $keyType = 'uuid';
 
     protected $fillable = ['phone', 'message', 'status', 'cost', 'user_id'];
+
+    /**
+     * Get the user that owns the sms.
+     * @return belongsTo
+     */
+    public function meter(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
