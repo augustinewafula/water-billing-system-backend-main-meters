9ca99faaeafccd260fa1614c2781829c687f7bf5
augustinewafula <augustinetreezy@outlook.com>
2022-03-20 07:24:52 +0300

    add account number to fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,8 @@ class User extends Authenticatable
         'meter_id',
         'password',
         'first_bill',
-        'account_balance'
+        'account_balance',
+        'account_number'
     ];
 
     /**
