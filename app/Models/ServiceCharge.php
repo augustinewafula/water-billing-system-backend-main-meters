6548c8f2de2c1572442afe064d0f14b6e6724bac
augustinewafula <augustinetreezy@outlook.com>
2022-03-22 14:55:16 +0300

    define relationship

--- app/Models/ServiceCharge.php
+++ app/Models/ServiceCharge.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ServiceCharge extends Model
 {
@@ -13,4 +14,13 @@ class ServiceCharge extends Model
     public $incrementing = false;
 
     protected $fillable = ['from', 'to', 'amount', 'meter_charge_id'];
+
+    /**
+     * Get meter charge that owns the service charge
+     * @return BelongsTo
+     */
+    public function meter_charge(): BelongsTo
+    {
+        return $this->belongsTo(MeterCharge::class);
+    }
 }
