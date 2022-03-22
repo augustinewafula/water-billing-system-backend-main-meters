9efa6f54c4519183936251a7af2a0b96ecd8bf37
augustinewafula <augustinetreezy@outlook.com>
2022-03-22 14:55:02 +0300

    add service_charge_in_percentage as fillable. define relationship

--- app/Models/MeterCharge.php
+++ app/Models/MeterCharge.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MeterCharge extends Model
 {
@@ -12,5 +13,14 @@ class MeterCharge extends Model
 
     public $incrementing = false;
 
-    protected $fillable = ['cost_per_unit', 'service_charge'];
+    protected $fillable = ['cost_per_unit', 'service_charge_in_percentage'];
+
+    /**
+     * Get the service charge for the meter charge.
+     * @return HasMany
+     */
+    public function service_charges(): HasMany
+    {
+        return $this->hasMany(ServiceCharge::class);
+    }
 }
