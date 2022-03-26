f2cbad854fbbcf0719d39681f5ff5113b9f293b3
augustinewafula <augustinetreezy@outlook.com>
2022-03-26 13:05:44 +0300

    create SwitchOnPaidMeter.php job

---
+++ app/Jobs/SwitchOnPaidMeter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Enums\MeterReadingStatus;
+use App\Enums\ValveStatus;
+use App\Models\MeterReading;
+use App\Traits\ToggleValveStatus;
+use Carbon\Carbon;
+use DB;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Log;
+use Throwable;
+
+class SwitchOnPaidMeter implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, ToggleValveStatus;
+
+    protected $meter;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($meter)
+    {
+        $this->meter = $meter;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws Throwable
+     */
+    public function handle(): void
+    {
+        $month_ago = Carbon::now()->subtract(1, 'month')->format('Y-m');
+        $meter = MeterReading::where('meter_id', $this->meter->id)
+            ->where('month', '>=', $month_ago)
+            ->whereStatus(MeterReadingStatus::Paid)
+            ->latest()
+            ->limit(1)
+            ->first();
+        if (!$meter) {
+            return;
+        }
+
+        try {
+            DB::beginTransaction();
+            $this->meter->update([
+                'valve_status' => ValveStatus::Open,
+            ]);
+            $this->toggleValve($this->meter, ValveStatus::Open);
+            DB::commit();
+        } catch (Throwable $th) {
+            DB::rollBack();
+            Log::error($th);
+        }
+    }
+}
