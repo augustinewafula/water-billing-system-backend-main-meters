2bf0a36153f13b1a05688ef75f1bb3d159df9cd6
augustinewafula <augustinetreezy@outlook.com>
2022-03-26 13:05:21 +0300

    add SwitchOffUnpaidMetersCommand to scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('queue:work --max-time=3000')->everyThreeMinutes()->withoutOverlapping();
+        $schedule->command('meters:switch-off-unpaid')->everyMinute()->withoutOverlapping();
         $schedule->command('meter-readings:send')->everyTwoMinutes();
         $schedule->command('meter-readings:get --type=daily')->dailyAt('23:50');
         $schedule->command('meter-readings:get --type=monthly')->lastDayOfMonth('23:50');
