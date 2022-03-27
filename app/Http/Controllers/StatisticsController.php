affd71c380eac939da7e6338f42329fdd74eca00
augustinewafula <augustinetreezy@outlook.com>
2022-03-27 17:43:17 +0300

    get token sum

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Meter;
 use App\Models\MeterBilling;
+use App\Models\MeterToken;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Log;
@@ -21,15 +22,19 @@ class StatisticsController extends Controller
             $meters = Meter::where('main_meter', false)
                 ->count();
             $billingsSum = MeterBilling::sum('amount_paid');
+            $tokenSum = MeterToken::select('meter_tokens.*', 'mpesa_transactions.TransAmount')
+                ->join('mpesa_transactions', 'mpesa_transactions.id', 'meter_tokens.mpesa_transaction_id')->sum('TransAmount');
         } catch (Throwable $throwable) {
             Log::error($throwable);
+            $response = ['message' => 'Something went wrong'];
+            return response()->json($response, 422);
         }
 
         return response()->json([
             'users' => $users,
             'main_meters' => $mainMeters,
             'meters' => $meters,
-            'revenue' => $billingsSum
+            'revenue' => $billingsSum + $tokenSum
         ]);
     }
 }
