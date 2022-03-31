a8e9799de3db389c1d6c5b83e3c3f845e92815c7
augustinewafula <augustinetreezy@outlook.com>
2022-03-31 22:58:31 +0300

    add resend tokens route

--- routes/api.php
+++ routes/api.php
@@ -71,6 +71,7 @@ Route::prefix('v1')->group(function () {
         Route::put('valve-status/{meter}', [MeterController::class, 'updateValveStatus']);
         Route::get('sms', [SmsController::class, 'index']);
         Route::post('sms', [SmsController::class, 'send']);
+        Route::post('meter-tokens-resend/{meter_token}', [MeterTokenController::class, 'resend']);
         Route::get('settings', [SettingController::class, 'index']);
         Route::get('download-users', [UserController::class, 'download']);
         Route::post('settings', [SettingController::class, 'update']);
