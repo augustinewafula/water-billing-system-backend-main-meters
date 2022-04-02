ce1ae7dfb624c998949719c69a9b76d9922c6f76
augustinewafula <augustinetreezy@outlook.com>
2022-04-02 10:57:51 +0300

    format meter reading sms

--- app/Traits/SendMeterReading.php
+++ app/Traits/SendMeterReading.php
@@ -22,7 +22,7 @@ trait SendMeterReading
         $due_date = Carbon::parse($meter_reading->bill_due_at)->format('d/m/Y');
         $bill_month = Carbon::parse($meter_reading->created_at)->isoFormat('MMMM YYYY');
         $units_consumed = $meter_reading->current_reading - $meter_reading->previous_reading;
-        $bill = $meter_reading->bill - $meter_reading->service_charge;
+        $bill = round($meter_reading->bill - $meter_reading->service_fee);
         $carry_forward_balance = 0;
         $over_paid_amount = 0;
         if ($user->account_balance < 0) {
@@ -33,12 +33,13 @@ trait SendMeterReading
         }
         $paybill_number = $meter->station->paybill_number;
         $account_number = $meter->user->account_number;
-        $total_outstanding = ($bill + $carry_forward_balance) - $over_paid_amount;
+        $total_outstanding = round(($bill + $carry_forward_balance) - $over_paid_amount);
+        $service_fee = round($meter_reading->service_fee);
         if ($total_outstanding < 0) {
             $total_outstanding = 0;
         }
 
-        $message = "Dear $user_name, your water billing for $bill_month is as follows:\nCurrent Reading: $meter_reading->current_reading\nPrevious reading: $meter_reading->previous_reading\nUnits consumed: $units_consumed\nBalance brought forward: Ksh $carry_forward_balance\nCredit Applied: Ksh $over_paid_amount\nService charge: Ksh $meter_reading->service_charge\nTotal outstanding: Ksh $total_outstanding\nDue date: $due_date\nPay via paybill number $paybill_number, account number $account_number";
+        $message = "Dear $user_name, your water billing for $bill_month is as follows:\nCurrent reading: $meter_reading->current_reading\nPrevious reading: $meter_reading->previous_reading\nUnits consumed: $units_consumed\nBalance brought forward: Ksh $carry_forward_balance\nCredit applied: Ksh $over_paid_amount\nStanding charge: Ksh $service_fee\nTotal outstanding: Ksh $total_outstanding\nDue date: $due_date\nPay via paybill number $paybill_number, account number $account_number";
         SendSMS::dispatch($user->phone, $message, $user->id);
         $meter_reading->update([
             'sms_sent' => true,
