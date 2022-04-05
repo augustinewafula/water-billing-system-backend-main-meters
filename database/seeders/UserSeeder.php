1c0b4fd4bfcde4794910a803f14f812dbd895813
augustinewafula <augustinetreezy@outlook.com>
2022-04-05 18:38:11 +0300

    update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,58 +3,43 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
+    public function createRoles(): void
+    {
+
+    }
+
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        Role::create([
-            'name' => 'user',
-            'guard_name' => 'api',
-        ]);
-        $supervisor = Role::create([
-            'name' => 'supervisor',
-            'guard_name' => 'api',
-        ]);
-        $admin = Role::create([
-            'name' => 'admin',
-            'guard_name' => 'api',
-        ]);
-        $super_admin = Role::create([
-            'name' => 'super-admin',
-            'guard_name' => 'api',
-        ]);
-
         $user = new User();
         $user->name = 'Nebstar Malash';
         $user->email = 'nebstarmalala@gmail.com';
         $user->password = bcrypt('qwertyuiop');
-        $user->first_bill = Carbon::now()->isoFormat('YYYY-MM');
-        $user->assignRole($supervisor);
+        $user->assignRole(Role::findByName('supervisor', 'api'));
         $user->save();
 
         $user = new User();
         $user->name = 'George Kimani';
         $user->email = 'george@progressive.co.ke';
         $user->password = bcrypt('qwertyuiop');
-        $user->first_bill = Carbon::now()->isoFormat('YYYY-MM');
-        $user->assignRole($supervisor, $admin);
+        $user->assignRole(Role::findByName('admin', 'api'));
         $user->save();
 
         $user = new User();
         $user->name = 'Augustine Wafula';
         $user->email = 'augustinetreezy@gmail.com';
         $user->password = bcrypt('qwertyuiop');
-        $user->first_bill = Carbon::now()->isoFormat('YYYY-MM');
-        $user->assignRole($supervisor, $admin, $super_admin);
+        $user->assignRole(Role::findByName('super-admin', 'api'));
         $user->save();
 
     }
