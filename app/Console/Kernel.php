569259e05f50b887528e4483baab23f9e85383c9
augustinewafula <augustinetreezy@outlook.com>
2022-04-10 17:25:41 +0300

    schedule monthly-service-charge:generate command to run monthly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,8 +20,9 @@ class Kernel extends ConsoleKernel
         $schedule->command('meters:switch-off-unpaid')->everyMinute()->withoutOverlapping();
         $schedule->command('meters:confirm-valve-status')->everyMinute()->withoutOverlapping();
         $schedule->command('meter-readings:send')->everyTwoMinutes();
-        $schedule->command('meter-readings:get --type=daily')->dailyAt('23:50');
-        $schedule->command('meter-readings:get --type=monthly')->lastDayOfMonth('23:50');
+        $schedule->command('meter-readings:get --type=daily')->dailyAt('23:30');
+        $schedule->command('meter-readings:get --type=monthly')->lastDayOfMonth();
+        $schedule->command('monthly-service-charge:generate')->monthly();
         $schedule->command('backup:clean --only-db')->twiceDaily(0, 12);
         $schedule->command('backup:run --only-db')->twiceDaily();
     }
