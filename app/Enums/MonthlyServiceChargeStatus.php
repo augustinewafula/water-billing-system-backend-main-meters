3435297bc9f0025f9426c247b974e24ee3753c1a
augustinewafula <augustinetreezy@outlook.com>
2022-04-10 16:11:48 +0300

    update status

--- app/Enums/MonthlyServiceChargeStatus.php
+++ app/Enums/MonthlyServiceChargeStatus.php
@@ -7,6 +7,7 @@ use BenSampo\Enum\Enum;
 
 final class MonthlyServiceChargeStatus extends Enum
 {
+    public const NotPaid = 0;
     public const Paid = 1;
     public const Balance = 2;
     public const OverPaid = 3;
