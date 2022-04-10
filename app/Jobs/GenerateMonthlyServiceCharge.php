4b7de26c51e18f60b10a489774a3f0e7c92fb575
augustinewafula <augustinetreezy@outlook.com>
2022-04-10 17:04:16 +0300

    create GenerateMonthlyServiceCharge job

---
+++ app/Jobs/GenerateMonthlyServiceCharge.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Setting;
+use App\Models\User;
+use App\Traits\GeneratesMonthlyServiceCharge;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class GenerateMonthlyServiceCharge implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, GeneratesMonthlyServiceCharge;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $users = User::role('user')
+            ->get();
+        $monthly_service_charge = Setting::where('key', 'monthly_service_charge')
+            ->first()
+            ->value;
+        foreach ($users as $user) {
+            $this->generate($user, $monthly_service_charge);
+        }
+    }
+
+}
