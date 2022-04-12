e5ff6b63c2da00c70f0d00e98c6250e669e5e4cc
augustinewafula <augustinetreezy@outlook.com>
2022-04-12 13:28:59 +0300

    register admin password reset route

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::prefix('v1')->group(function () {
         Route::prefix('admin')->group(function () {
             Route::post('login', [AuthController::class, 'initiateAdminLogin']);
             Route::post('password/email', [ForgotPasswordController::class, 'getResetToken']);
+            Route::post('password/reset', [ForgotPasswordController::class, 'submitResetPasswordForm']);
             Route::group(['middleware' => ['role:super-admin|admin|supervisor', 'auth:api']], function () {
                 Route::get('profile', [AuthController::class, 'user']);
                 Route::put('profile/{user}', [AuthController::class, 'update']);
