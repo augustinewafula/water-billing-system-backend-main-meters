ff0514580a17ffe4f39f6509850705439bc84232
augustinewafula <augustinetreezy@outlook.com>
2022-04-13 09:03:58 +0300

    validate MeterTokenRequest

---
+++ app/Http/Requests/CreateMeterTokenRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\mpesaTransactionNotConsumed;
+use App\Rules\prepaidMeter;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateMeterTokenRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'meter_id' => ['required', 'string', 'exists:meters,id', new prepaidMeter()],
+            'mpesa_transaction_reference' => ['required', 'string', 'exists:mpesa_transactions,TransID', new mpesaTransactionNotConsumed()],
+        ];
+    }
+}
