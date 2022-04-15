833d9fa26c599684706a29fa1a0e16790b68ee6b
augustinewafula <augustinetreezy@outlook.com>
2022-04-15 10:47:47 +0300

    move unresolvedTransactionIndex to its controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -48,14 +48,6 @@ class TransactionController extends Controller
 
     }
 
-    public function unresolvedTransactionIndex(): JsonResponse
-    {
-        return response()->json(UnresolvedMpesaTransaction::select('unresolved_mpesa_transactions.reason', 'mpesa_transactions.id', 'mpesa_transactions.TransID as transaction_reference', 'mpesa_transactions.TransAmount as amount', 'mpesa_transactions.BillRefNumber as account_number', 'mpesa_transactions.MSISDN as phone_number', 'mpesa_transactions.created_at as transaction_time')
-            ->join('mpesa_transactions', 'mpesa_transactions.id', 'unresolved_mpesa_transactions.mpesa_transaction_id')
-            ->latest('transaction_time')
-            ->get());
-    }
-
     /**
      * Display the specified resource.
      *
