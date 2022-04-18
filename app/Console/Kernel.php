b7e236860e5856f61b5c39913af66d7593266ee1
augustinewafula <augustinetreezy@outlook.com>
2022-04-18 11:59:20 +0300

    schedule model:prune command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('meters:switch-off-unpaid')->everyMinute()->withoutOverlapping();
         $schedule->command('meters:confirm-valve-status')->hourly()->withoutOverlapping();
         $schedule->command('meter-readings:send')->everyTwoMinutes();
-        $schedule->command('meter-readings:get --type=daily')->dailyAt('23:30');
+        $schedule->command('meter-readings:get --type=daily')->daily();
+        $schedule->command('model:prune')->daily();
         $schedule->command('meter-readings:get --type=monthly')->lastDayOfMonth();
         $schedule->command('monthly-service-charge:generate')->monthly();
         $schedule->command('backup:clean --only-db')->twiceDaily(0, 12);
