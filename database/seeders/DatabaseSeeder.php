e0d1840717ac83228f21eb2db851650fdd37c4d1
augustinewafula <augustinetreezy@outlook.com>
2022-04-18 15:41:00 +0300

    call AlertContactSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             MeterStationSeeder::class,
             MeterChargeSeeder::class,
             SettingSeeder::class,
+            AlertContactSeeder::class,
         ]);
     }
 }
