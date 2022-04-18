ef807acb8500c3571dd919030b4ea478c3456bfa
augustinewafula <augustinetreezy@outlook.com>
2022-04-18 15:41:47 +0300

    create CreateAlertContactController

--- app/Http/Controllers/AlertContactController.php
+++ app/Http/Controllers/AlertContactController.php
@@ -2,64 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateAlertContactRequest;
 use App\Models\AlertContact;
-use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AlertContactController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $current_user_id = auth()->guard('api')->user()->id;
+        $alert_contacts = AlertContact::where('user_id', $current_user_id)
+            ->paginate(10);
+        return response()->json($alert_contacts);
+
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param CreateAlertContactRequest $request
+     * @return Response
      */
-    public function store(Request $request)
+    public function store(CreateAlertContactRequest $request)
     {
-        //
+        $current_user_id = auth()->guard('api')->user()->id;
+        return AlertContact::create([
+            'user_id' => $current_user_id,
+            'type' => $request->type,
+            'value' => $request->value
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\AlertContact  $alertContact
-     * @return \Illuminate\Http\Response
-     */
-    public function show(AlertContact $alertContact)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\AlertContact  $alertContact
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param AlertContact $alertContact
+     * @return JsonResponse
      */
     public function update(Request $request, AlertContact $alertContact)
     {
-        //
+        $alertContact->create([
+            'type' => $request->type,
+            'value' => $request->value
+        ]);
+        return response()->json($alertContact);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\AlertContact  $alertContact
-     * @return \Illuminate\Http\Response
+     * @param AlertContact $alertContact
+     * @return JsonResponse
      */
     public function destroy(AlertContact $alertContact)
     {
-        //
+        $alertContact->delete();
+        return response()->json('deleted');
     }
 }
