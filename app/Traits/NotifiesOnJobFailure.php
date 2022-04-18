c745f1ff70f976e7d3d8deccfbc2f4687e4ad7ce
augustinewafula <augustinetreezy@outlook.com>
2022-04-18 19:45:31 +0300

    create NotifiesOnJobFailure trait

---
+++ app/Traits/NotifiesOnJobFailure.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+use App\Mail\CriticalNotification;
+use Mail;
+use Throwable;
+
+trait NotifiesOnJobFailure
+{
+    /**
+     * Handle a job failure.
+     *
+     * @param Throwable $exception
+     * @return void
+     */
+    public function failed(Throwable $exception): void
+    {
+        $to = env('CRITICAL_NOTIFICATIONS_EMAIL');
+        if ($to){
+            Mail::to($to)
+                ->send(new CriticalNotification($exception));
+
+        }
+    }
+}
