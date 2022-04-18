af2598b1a4947ec523caf2524bde47af64e598b3
augustinewafula <augustinetreezy@outlook.com>
2022-04-18 15:41:26 +0300

    create CreateAlertContactRequest.php

---
+++ app/Http/Requests/CreateAlertContactRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\AlertContactTypes;
+use BenSampo\Enum\Rules\EnumValue;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateAlertContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => ['required', new EnumValue(AlertContactTypes::class, false)],
+            'value' => ['required', 'string']
+        ];
+    }
+}
