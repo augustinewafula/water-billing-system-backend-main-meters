013ab85ee322063e89f6b6aea429453985bc68e3
augustinewafula <augustinetreezy@outlook.com>
2022-04-25 18:46:46 +0300

    create SetsEnvironmentalValue.php

---
+++ app/Traits/SetsEnvironmentalValue.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Traits;
+
+trait SetsEnvironmentalValue
+{
+    public function setEnvironmentValue($envKey, $envValue): void
+    {
+        $envFile = app()->environmentFilePath();
+        $str = file_get_contents($envFile);
+
+        $oldValue = env($envKey);
+
+        $str = str_replace("{$envKey}={$oldValue}", "{$envKey}={$envValue}\n", $str);
+
+        $fp = fopen($envFile, 'wb');
+        fwrite($fp, $str);
+        fclose($fp);
+    }
+}
