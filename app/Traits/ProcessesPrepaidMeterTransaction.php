3a67101a1dd25fe03694dc3ede58ca51aed4085e
augustinewafula <augustinetreezy@outlook.com>
2022-04-28 11:51:10 +0300

    use GeneratesMeterToken.php

--- app/Traits/ProcessesPrepaidMeterTransaction.php
+++ app/Traits/ProcessesPrepaidMeterTransaction.php
@@ -18,7 +18,7 @@ use Throwable;
 
 trait ProcessesPrepaidMeterTransaction
 {
-    use AuthenticatesMeter, CalculatesBill, CalculatesUserTotalAmount, SetsEnvironmentalValue;
+    use AuthenticatesMeter, CalculatesBill, CalculatesUserTotalAmount, GeneratesMeterToken;
 
     protected $baseUrl = 'http://www.shometersapi.stronpower.com/api/';
 
@@ -111,36 +111,4 @@ trait ProcessesPrepaidMeterTransaction
         }
     }
 
-    /**
-     * @throws JsonException
-     */
-    public function generateMeterToken($meter_number, $amount): ?string
-    {
-        $api_token = env('PREPAID_METER_API_TOKEN');
-        if (empty($api_token)){
-            $api_token = $this->loginPrepaidMeter();
-        }
-        $prepaid_meter_charges = MeterCharge::where('for', 'prepay')
-            ->first();
-
-        $response = Http::post($this->baseUrl . 'vending', [
-                'CustomerId' => $meter_number,
-                'MeterId' => $meter_number,
-                'Price' => (int)$prepaid_meter_charges->cost_per_unit,
-                'Rate' => 1,
-                'Amount' => (int)$amount,
-                'AmountTmp' => 'KES',
-                'Company' => env('PREPAID_METER_COMPANY'),
-                'Employee' => '0000',
-                'ApiToken' => $api_token,
-            ]);
-        if ($response->successful()) {
-            Log::info('vending response:' . $response->body());
-            if ($response->body() === ''){
-                $this->setEnvironmentValue('PREPAID_METER_API_TOKEN', null);
-            }
-            return json_decode($response->body(), false, 512, JSON_THROW_ON_ERROR);
-        }
-        return null;
-    }
 }
