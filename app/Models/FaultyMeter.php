76e13e2d0bc2350687a0084b7528b38b899433cc
augustinewafula <augustinetreezy@outlook.com>
2022-04-28 22:18:58 +0300

    define belongsTo meter relationship

--- app/Models/FaultyMeter.php
+++ app/Models/FaultyMeter.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use App\Traits\ClearsResponseCache;
 use App\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FaultyMeter extends Model
@@ -20,4 +22,23 @@ class FaultyMeter extends Model
     protected $dateFormat = 'Y-m-d H:i:s.u';
 
     protected $fillable = ['meter_id', 'fault_type'];
+
+    /**
+     * Get meter that owns the faulty meter
+     * @return BelongsTo
+     */
+    public function meter(): BelongsTo
+    {
+        return $this->belongsTo(Meter::class);
+    }
+
+    /**
+     * Get the prunable model query.
+     *
+     * @return Builder
+     */
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<=', now()->subMonth());
+    }
 }
