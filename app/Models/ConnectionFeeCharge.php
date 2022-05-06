008178061f8dc5ece865ff4798b2018a6ed6d6bb
augustinewafula <augustinetreezy@outlook.com>
2022-05-06 17:04:05 +0300

    add belongs to station relationship

--- app/Models/ConnectionFeeCharge.php
+++ app/Models/ConnectionFeeCharge.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ConnectionFeeCharge extends Model
@@ -19,6 +20,15 @@ class ConnectionFeeCharge extends Model
     protected $dateFormat = 'Y-m-d H:i:s.u';
 
     protected $fillable = ['connection_fee', 'connection_fee_monthly_installment', 'station_id'];
+
+    /**
+     * Get meter station that owns the connection fee charge
+     * @return BelongsTo
+     */
+    public function station(): BelongsTo
+    {
+        return $this->belongsTo(MeterStation::class);
+    }
 
     /**
      * Get the prunable model query.
