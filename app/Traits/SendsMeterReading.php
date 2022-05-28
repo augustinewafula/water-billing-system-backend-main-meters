4a2ab39c5bc0b4f90504f595c3816d960b04ed67
augustinewafula <augustinetreezy@outlook.com>
2022-05-28 18:02:29 +0300

    refactor SendsMeterReading.php

--- app/Traits/SendsMeterReading.php
+++ app/Traits/SendsMeterReading.php
@@ -2,61 +2,26 @@
 
 namespace App\Traits;
 
-use App\Jobs\SendSMS;
-use App\Mail\CriticalNotification;
-use App\Mail\MeterReadings;
-use App\Models\Meter;
-use App\Models\MeterBilling;
 use App\Models\User;
 use Carbon\Carbon;
-use Mail;
 
 trait SendsMeterReading
 {
-    use NotifiesUser;
+    use NotifiesUser, ConstructsMeterReadingMessage;
 
     public function sendMeterReading($meter_reading): void
     {
         $user = User::where('meter_id', $meter_reading->meter_id)
-            ->first();
-        if (!$user) {
-            return;
-        }
-        $meter = Meter::with('station', 'user')
-            ->find($meter_reading->meter_id);
-        $user_name = $user->name;
-        $due_date = Carbon::parse($meter_reading->bill_due_at)->format('d/m/Y');
+            ->firstOrFail();
+
         $bill_month = Carbon::parse($meter_reading->month)->isoFormat('MMMM YYYY');
-        $units_consumed = $meter_reading->current_reading - $meter_reading->previous_reading;
-        $bill = round($meter_reading->bill - $meter_reading->service_fee);
-        $carry_forward_balance = 0;
-        $credit = 0;
-
-        if ($user->account_balance < 0) {
-            $remaining_amount = abs($user->account_balance) - $meter_reading->bill;
-            if ($remaining_amount < 0){
-                $credit = abs($remaining_amount);
-            }else{
-                $carry_forward_balance = $remaining_amount;
-            }
-        }
-        if ($meter_billing = MeterBilling::where('meter_reading_id', $meter_reading->id)->first()){
-            $credit = $meter_billing->credit;
-        }
-
-        $paybill_number = $meter->station->paybill_number;
-        $account_number = $meter->user->account_number;
-        $total_outstanding = round(($meter_reading->bill + $carry_forward_balance) - $credit);
-        $service_fee = round($meter_reading->service_fee);
-        if ($total_outstanding < 0) {
-            $total_outstanding = 0;
-        }
-
-        $message = "Hello $user_name, your water billing for $bill_month is as follows:\nCurrent reading: $meter_reading->current_reading\nPrevious reading: $meter_reading->previous_reading\nUnits consumed: $units_consumed\nBalance brought forward: Ksh $carry_forward_balance\nCredit applied: Ksh $credit\nStanding charge: Ksh $service_fee\nTotal outstanding: Ksh $total_outstanding\nDue date: $due_date\nPay via paybill number $paybill_number, account number $account_number";
+        $message = $this->constructMeterReadingMessage($meter_reading, $user);
 
         $this->notifyUser((object)['message' => $message, 'bill_month' =>$bill_month], $user, 'meter readings');
         $meter_reading->update([
             'sms_sent' => true,
         ]);
     }
+
+
 }
