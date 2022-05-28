7f85e90b4d6377a3383d57e0c352177d8e0b5f06
augustinewafula <augustinetreezy@outlook.com>
2022-05-28 18:01:30 +0300

    create ConstructsMeterReadingMessage.php trait

---
+++ app/Traits/ConstructsMeterReadingMessage.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Meter;
+use App\Models\MeterBilling;
+use Carbon\Carbon;
+
+trait ConstructsMeterReadingMessage
+{
+    /**
+     * @param $meter_reading
+     * @param $user
+     * @return string
+     */
+    private function constructMeterReadingMessage($meter_reading, $user): string
+    {
+        $meter = Meter::with('station', 'user')
+            ->find($meter_reading->meter_id);
+        $user_name = $user->name;
+        $due_date = Carbon::parse($meter_reading->bill_due_at)->format('d/m/Y');
+        $bill_month = Carbon::parse($meter_reading->month)->isoFormat('MMMM YYYY');
+        $units_consumed = $meter_reading->current_reading - $meter_reading->previous_reading;
+        $bill = round($meter_reading->bill - $meter_reading->service_fee);
+        $carry_forward_balance = 0;
+        $credit = 0;
+
+        $user_total_debt = $user->unaccounted_debt;
+        \Log::info("user_total_debt: $user_total_debt");
+        if ($user->account_balance < 0) {
+            $user_total_debt += abs($user->account_balance);
+            $remaining_amount = abs($user->account_balance) - $meter_reading->bill;
+            if ($remaining_amount < 0) {
+                $credit = abs($remaining_amount);
+            } else {
+                $carry_forward_balance = $remaining_amount;
+            }
+        }
+        if ($meter_billing = MeterBilling::where('meter_reading_id', $meter_reading->id)->first()) {
+            $credit = $meter_billing->credit + $meter_reading->amount_paid;
+        }
+        \Log::info("credit: $credit");
+        $user_total_debt -= $carry_forward_balance;
+        \Log::info("meter_reading_bill: $meter_reading->bill");
+        \Log::info("user_total_debt: $user_total_debt");
+
+        $paybill_number = $meter->station->paybill_number;
+        $account_number = $meter->user->account_number;
+        $total_outstanding = round(($meter_reading->bill + $user_total_debt) - $credit);
+        $service_fee = round($meter_reading->service_fee);
+        if ($total_outstanding < 0) {
+            $total_outstanding = 0;
+        }
+
+        $message = "Hello $user_name, your water billing for $bill_month is as follows:\nCurrent reading: $meter_reading->current_reading\nPrevious reading: $meter_reading->previous_reading\nUnits consumed: $units_consumed\nBalance brought forward: Ksh $carry_forward_balance\nCredit applied: Ksh $credit\nStanding charge: Ksh $service_fee\nTotal outstanding: Ksh $user_total_debt\nDue date: $due_date\nPay via paybill number $paybill_number, account number $account_number";
+        return $message;
+    }
+
+}
