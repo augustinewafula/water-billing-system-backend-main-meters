f9152aa38531f503edc59a2d7f232c147bf3c51c
augustinewafula <augustinetreezy@outlook.com>
2022-07-10 13:40:17 +0300

    remove email footer details

--- resources/views/emails/meterTokens.blade.php
+++ resources/views/emails/meterTokens.blade.php
@@ -62,12 +62,6 @@
                     <td style="height:20px;">&nbsp;</td>
                 </tr>
                 <tr>
-                    <td style="text-align:center;">
-                        <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">
-                            &copy; <strong>www.water.progressive.co.ke</strong></p>
-                    </td>
-                </tr>
-                <tr>
                     <td style="height:80px;">&nbsp;</td>
                 </tr>
             </table>
