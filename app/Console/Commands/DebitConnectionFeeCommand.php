4f7b3dc0ab960ad6f132f06c9cf1a45af9778c22
augustinewafula <augustinetreezy@outlook.com>
2022-07-23 18:41:06 +0300

    create command to debit connection fee to user account

---
+++ app/Console/Commands/DebitConnectionFeeCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\ConnectionFee;
+use App\Services\ConnectionFeeService;
+use Illuminate\Console\Command;
+
+class DebitConnectionFeeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:debit-connection-fee';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Debit monthly connection fee of current month for all users who have not yet fully paid.';
+
+    /**
+     * Execute the console command.
+     *
+     * @param ConnectionFeeService $connectionFeeService
+     * @return int
+     */
+    public function handle(ConnectionFeeService $connectionFeeService): int
+    {
+        $connectionFees = ConnectionFee::notAddedToUserTotalDebt()
+            ->currentMonth()
+            ->notPaid()
+            ->orWhere
+            ->hasBalance()
+            ->get();
+        \Log::info('Found ' . $connectionFees->count() . ' connection fees to debit.');
+        foreach ($connectionFees as $connectionFee) {
+            $connectionFeeService->addConnectionFeeBillToUserAccount($connectionFee);
+        }
+        return 0;
+    }
+}
