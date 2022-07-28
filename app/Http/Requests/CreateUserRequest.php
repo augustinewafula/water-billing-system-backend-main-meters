799f271b16531ca0658c5912336fdfc8cbd28756
augustinewafula <augustinetreezy@outlook.com>
2022-07-28 15:57:54 +0300

    add nullable rule number_of_months_to_pay_connection_fee field

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -31,7 +31,7 @@ class CreateUserRequest extends FormRequest
             'meter_id' => ['required', 'string', 'exists:meters,id', 'unique:users,meter_id', 'max:50'],
             'should_pay_connection_fee' => ['required', 'boolean'],
             'connection_fee' => ['required_if:should_pay_connection_fee,true', 'nullable', 'numeric', 'min:1'],
-            'number_of_months_to_pay_connection_fee' => ['required_if:should_pay_connection_fee,true', 'numeric', 'min:1'],
+            'number_of_months_to_pay_connection_fee' => ['required_if:should_pay_connection_fee,true', 'nullable', 'numeric', 'min:1'],
             'first_connection_fee_on' => ['required_if:should_pay_connection_fee,true', 'nullable', 'date_format:Y-m'],
             'use_custom_charges_for_cost_per_unit' => ['required', 'boolean'],
             'cost_per_unit' => ['required_if:use_custom_charges_for_cost_per_unit,true', 'nullable', 'numeric'],
