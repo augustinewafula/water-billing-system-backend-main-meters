aa1397d596936fe5af2aa71ccc3d5a9b58be395b
augustinewafula <augustinetreezy@outlook.com>
2022-09-18 16:01:07 +0300

    add supllementary message

--- app/Jobs/SwitchOffUnpaidMeters.php
+++ app/Jobs/SwitchOffUnpaidMeters.php
@@ -47,30 +47,30 @@ class SwitchOffUnpaidMeters implements ShouldQueue
      */
     public function handle(): void
     {
-        $unpaid_meters = MeterReading::with(['meter' => function ($query) {
+        $unpaid_meter_readings = MeterReading::with(['meter' => function ($query) {
             $query->whereValveStatus(ValveStatus::OPEN)
                 ->orWhere('valve_status', null);
-        }])
+            }])
             ->whereDate('actual_meter_disconnection_on', '<=', now())
             ->where(function ($query) {
                 $query->whereStatus(PaymentStatus::NOT_PAID)
                     ->orWhere('status', PaymentStatus::PARTIALLY_PAID);
             })
             ->get();
-        foreach ($unpaid_meters as $unpaid_meter) {
+        foreach ($unpaid_meter_readings as $unpaid_meter_reading) {
             try {
-                if (!$unpaid_meter->meter) {
+                if (!$unpaid_meter_reading->meter) {
                     continue;
                 }
-                $meter = Meter::with('user', 'station')->findOrFail($unpaid_meter->meter->id);
+                $meter = Meter::with('user', 'station')->findOrFail($unpaid_meter_reading->meter->id);
                 if (!$meter->user) {
                     continue;
                 }
                 $paybill_number = $meter->station->paybill_number;
                 $account_number = $meter->user->account_number;
                 $first_name = explode(' ', trim($meter->user->name))[0];
-                $meter_reading_debt = $this->calculateUserMeterReadingDebt($unpaid_meter->meter->id);
-                $connection_fee_debt = $this->calculateUserConnectionFeeDebt($unpaid_meter->meter->user->id);
+                $meter_reading_debt = $this->calculateUserMeterReadingDebt($unpaid_meter_reading->meter->id);
+                $connection_fee_debt = $this->calculateUserConnectionFeeDebt($unpaid_meter_reading->meter->user->id);
                 $total_debt = $meter_reading_debt + $connection_fee_debt;
 
                 $meter_reading_debt_formatted = number_format($meter_reading_debt);
@@ -80,23 +80,34 @@ class SwitchOffUnpaidMeters implements ShouldQueue
                 if ($total_debt <= 200){
                     continue;
                 }
-                Log::info('Auto switching off unpaid meter id: '. $unpaid_meter->meter->id);
+                Log::info('Auto switching off unpaid meter id: '. $unpaid_meter_reading->meter->id);
 
                 $debt_breakdown = '';
                 if ($connection_fee_debt > 0) {
                     $debt_breakdown .= "\nMeter Billing Debt: Ksh {$meter_reading_debt_formatted} \nConnection Fee Debt: Ksh $connection_fee_debt_formatted.";
                 }
 
-                $message = "Hello $first_name, your water meter is going to be disconnected effective immediately. $debt_breakdown \nTotal outstanding Debt: Ksh $total_debt_formatted \nPay via paybill number $paybill_number, account number $account_number";
+                $supplementary_message = 'your water meter is going to be disconnected effective immediately. ';
+
+                if ($unpaid_meter_reading->disconnection_sms_sent) {
+                    $supplementary_message = 'please pay the remaining pending debt to be reconnected. ';
+                }
+
+                $message = "Hello $first_name, $supplementary_message $debt_breakdown \nTotal outstanding Debt: Ksh $total_debt_formatted \nPay via paybill number $paybill_number, account number $account_number";
 
                 $meter->update([
                     'valve_status' => ValveStatus::CLOSED,
                 ]);
-                if ($unpaid_meter->meter->mode !== MeterMode::MANUAL) {
+                if ($unpaid_meter_reading->meter->mode !== MeterMode::MANUAL) {
                     Log::info("disconnecting user {$meter->user->account_number}. Total debt is {$total_debt_formatted}");
                     $this->toggleValve($meter, ValveStatus::CLOSED);
                 }
                 $this->notifyUser((object)['message' => $message, 'title' => 'Water disconnection'], $meter->user, 'general');
+
+                $meter_reading = MeterReading::findOrFail($unpaid_meter_reading->id);
+                $meter_reading->update([
+                    'disconnection_sms_sent' => true,
+                ]);
             } catch (Throwable $th) {
                 Log::error($th);
             }
