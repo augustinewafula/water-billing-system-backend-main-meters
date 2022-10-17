fda2f0a9ad50cc3d0bff4d96e2bddebe043caa67
augustinewafula <augustinetreezy@outlook.com>
2022-10-17 12:52:28 +0300

    define user relationship

--- app/Models/ConnectionFeePayment.php
+++ app/Models/ConnectionFeePayment.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use \Znck\Eloquent\Traits\BelongsToThrough;
 
 class ConnectionFeePayment extends Model
 {
-    use HasFactory, HasUuid, SoftDeletes, MassPrunable;
+    use HasFactory, HasUuid, SoftDeletes, MassPrunable, BelongsToThrough;
 
     public $incrementing = false;
 
@@ -29,6 +30,14 @@ class ConnectionFeePayment extends Model
     public function connection_fee(): BelongsTo
     {
         return $this->belongsTo(ConnectionFee::class);
+    }
+
+    /**
+     * Get user that owns the user connection fee payment
+     */
+    public function user(): \Znck\Eloquent\Relations\BelongsToThrough
+    {
+        return $this->belongsToThrough(User::class, ConnectionFee::class);
     }
 
     /**
