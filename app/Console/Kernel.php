25b2f87bc9a2ff8cdf16801c123bbff4e51af0d7
augustinewafula <augustinetreezy@outlook.com>
2022-12-25 15:55:57 +0300

    update scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('queue:restart')->everyFifteenMinutes();
-        $schedule->command('queue:work')->everyThreeMinutes()->withoutOverlapping(60);
+        $schedule->command('queue:work')->everyThreeMinutes()->withoutOverlapping(15);
         $schedule->command('meters:switch-off-unpaid')->everyThreeMinutes()->withoutOverlapping();
         $schedule->command('meters:switch-on-paid')->everyTwoMinutes()->withoutOverlapping();
         $schedule->command('meters:confirm-valve-status')->everyThirtyMinutes()->withoutOverlapping();
