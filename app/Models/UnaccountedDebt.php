020f5bfc3bd3879e8ba3cd1489ea4c0c71d42fa0
augustinewafula <augustinetreezy@outlook.com>
2023-09-21 15:01:21 +0300

    append initial_unaccounted_debt attribute

--- app/Models/UnaccountedDebt.php
+++ app/Models/UnaccountedDebt.php
@@ -24,6 +24,25 @@ class UnaccountedDebt extends Model
 
     protected $fillable = ['user_id', 'amount_paid', 'amount_deducted', 'amount_remaining', 'mpesa_transaction_id'];
 
+    protected $appends = [
+        'initial_unaccounted_debt',
+    ];
+
+    public function getInitialUnaccountedDebtAttribute(): float
+    {
+        // Query only once for the sum of amount_paid for all UnaccountedDebt belonging to the same user
+        $totalAmountPaid = self::where('user_id', $this->user_id)
+            ->sum('amount_paid');
+
+        // Retrieve the unaccounted_debt from the User model
+        $userUnaccountedDebt = $this->user->unaccounted_debt;
+
+        // Calculate the initial unaccounted debt
+        $initialUnaccountedDebt = $totalAmountPaid + $userUnaccountedDebt;
+
+        return round($initialUnaccountedDebt, 1);
+    }
+
     /**
      * Get the user that owns the sms.
      * @return belongsTo
